5c52aff4d14583dc8476e2d8780b7c0b26110fc1
Sanshit Sagar <>
2021-01-19 18:08:02 -0800

    added syntax highlighting with prism

--- src/containers/Notes.js
+++ src/containers/Notes.js
@@ -9,12 +9,17 @@ import "./Notes.css";
 import { s3Upload } from "../libs/awsLib"; 
 import { Card, Button } from "react-bootstrap"; 
 
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { shadesOfPurple } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
 export default function Notes() {
   const file = useRef(null);
   const { id } = useParams();
   const history = useHistory();
   const [note, setNote] = useState(null);
   const [content, setContent] = useState("");
+  const [openEditor, setOpenEditor] = useState(false); 
+  const [programmingLanguage, setProgrammingLanguage] = useState("javascript")
   const [isLoading, setIsLoading] = useState(false); 
   const [isDeleting, setIsDeleting] = useState(false); 
 
@@ -95,6 +100,14 @@ export default function Notes() {
   function deleteNote() {
     return API.del("notes", `/notes/${id}`);
   }
+
+  function Codeblock() {
+    return (
+      <SyntaxHighlighter language={programmingLanguage} style={shadesOfPurple}> 
+        { content }
+      </SyntaxHighlighter>
+    );
+  }; 
   
   async function handleDelete(event) {
     event.preventDefault();
@@ -117,6 +130,26 @@ export default function Notes() {
       setIsDeleting(false);
     }
   }
+
+  function handleEdit(event) {
+    event.preventDefault(); 
+
+    setOpenEditor(!openEditor); 
+  }
+
+  const LanguageSelector = () => {
+    return (
+      <Form.Group controlId="LanguageSelector">
+        <Form.Label>Language</Form.Label>
+        <Form.Control as="select">
+          <option>Java</option>
+          <option>C++</option>
+          <option>JavaScript</option>
+          <option>Python</option>
+        </Form.Control>
+      </Form.Group> 
+    ); 
+  }
   
   return (
     <div className="Notes">
@@ -124,73 +157,77 @@ export default function Notes() {
         <Form onSubmit={handleSubmit}>
 
          <div className="Editor">
-            <Form.Group controlId="content">
+           <div className = "editorPane"> 
+            {openEditor && <Form.Group controlId="content">
               <Form.Control
                 as="textarea"
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
               />
-            </Form.Group>
+            </Form.Group> }
             
+            {!openEditor && <Codeblock />} 
+            <Button
+              block
+              size="sm"
+              variant="outline-success"
+              isLoading={isLoading}
+              onClick={handleEdit}
+            >
+              {!openEditor && 'Edit'}
+              {openEditor && 'Done'}
+            </Button>
+            </div> 
+
             <div className="sidePane"> 
-            <Card>
-              <Card.Header as="h5">Settings</Card.Header>
-              <Card.Body>
-                <Card.Title>Attachments</Card.Title>
-                {/* <Card.Text>
-                  With supporting text below as a natural lead-in to additional content.
-                </Card.Text> */}
-                {/* <Button variant="primary">Save</Button> */}
+              <Card>
+                <Card.Header as="h5">Settings</Card.Header>
+                <Card.Body>
+                  <Card.Title>Attachments</Card.Title>
               
-              <Form.Group controlId="file">
-                {/* <Form.Label>Attachment</Form.Label> */}
-                {note.attachment && (
-                  <p>
-                    <a
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href={note.attachmentURL}
-                    >
-                      {formatFilename(note.attachment)}
-                    </a>
-                  </p>
-                )}
-                <Form.Control onChange={handleFileChange} type="file" />
-              </Form.Group>
-             
-              <Form.Group controlId="LanguageSelector">
-                <Form.Label>Language</Form.Label>
-                <Form.Control as="select">
-                  <option>Java</option>
-                  <option>C++</option>
-                  <option>JavaScript</option>
-                  <option>Python</option>
-                </Form.Control>
-              </Form.Group> 
-              </Card.Body>
-            </Card>
+                
+                  <Form.Group controlId="file">
+                    {note.attachment && (
+                      <p>
+                        <a
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          href={note.attachmentURL}
+                        >
+                          {formatFilename(note.attachment)}
+                        </a>
+                      </p>
+                    )}
+                    <Form.Control onChange={handleFileChange} type="file" />
+                  </Form.Group>
+                
+                 <LanguageSelector /> 
+
+                  <div className = "submission"> 
+                  <LoaderButton
+                    block
+                    size="lg"
+                    type="submit"
+                    isLoading={isLoading}
+                    disabled={!validateForm()}
+                  >
+                      Save
+                  </LoaderButton>
+                  <LoaderButton
+                    block
+                    size="lg"
+                    variant="outline-danger"
+                    onClick={handleDelete}
+                    isLoading={isDeleting}
+                  >
+                      Delete
+                  </LoaderButton>
+                  </div>
+
+                </Card.Body>
+              </Card>
             </div>
-
-          </div>
-
-          <LoaderButton
-            block
-            size="sm"
-            type="submit"
-            isLoading={isLoading}
-            disabled={!validateForm()}
-          >
-            Save
-          </LoaderButton>
-          <LoaderButton
-            block
-            size="sm"
-            variant="danger"
-            onClick={handleDelete}
-            isLoading={isDeleting}
-          >
-            Delete
-          </LoaderButton>
+            </div> 
         </Form>
       )}
     </div>
